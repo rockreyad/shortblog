1000d4396c7fdb4e71e93351ee79f38ea495e80e
Rockreyad <rockreyad@gmail.com>
2022-05-28 14:12:33 +0600

    finalized

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -16,22 +16,50 @@ const Blog = () => {
   }, []);
 
   return (
-    <div>
-      <Link to={"/"}>
-        <span>&#8592;</span>Go back
+    <div className="p-2">
+      <Link
+        to={"/"}
+        className="flex justify-start items-center space-x-2 hover:bg-gray-400 bg-gray-600 w-fit px-2 rounded-md text-white"
+      >
+        <span className=" text-xl font-semibold">&#8592;</span>
+        <p>Go back</p>
       </Link>
 
       {blog ? (
         <div className="">
           <header>
-            <p className="">Published {blog.createdAt}</p>
-            <h1>{blog.title}</h1>
-            <div>
+            <p className="mt-5"></p>
+
+            <img
+              src={blog.cover}
+              alt="cover"
+              className="rounded-lg shadow-lg shadow-gray-600 my-2"
+            />
+            <div className="flex space-x-2">
               {blog.subCategory.map((category, index) => (
-                <div>
+                <div className="flex">
                   <Chip key={index} label={category} />
                 </div>
               ))}
+            </div>
+
+            <h1 className="text-2xl font-semibold">{blog.title}</h1>
+            <div className="flex justify-between bg-gray-100 py-2 px-0.5 rounded-sm">
+              <h3>
+                Author:{" "}
+                <span className="font-semibold text-gray-600">
+                  {blog.authorName}
+                </span>
+              </h3>
+              <h3>
+                Published{" "}
+                <span className="font-semibold text-gray-600">
+                  {blog.createdAt}
+                </span>
+              </h3>
+            </div>
+            <div className="leading-8 text-lg bg-indigo-50 shadow-sm py-2">
+              {blog.description}
             </div>
           </header>
         </div>
